d2ad5dc19352b3aa393662385c0993ff0ab25892
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-03 13:18:00 +0530

    RNN: add vanilla rnn

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from lstm!")
+import torch
+import torch.nn as nn
+import math
 
 
-if __name__ == "__main__":
-    main()
+class RNN(nn.Module):
+
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.W_xh = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.W_hh = nn.Parameter(torch.randn(hidden_size, hidden_size))
+
+        self.b_h = nn.Parameter(torch.randn(hidden_size))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-std, std)
+
+    def forward(self, x_t, h_prev):
+        h_t = torch.tanh(x_t @ self.W_xh + h_prev @ self.W_hh + self.b_h)
+        return h_t
