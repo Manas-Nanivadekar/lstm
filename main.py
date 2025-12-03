e0a7e1ab370585daab2ff518a4aec6dbc6a84be7
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-12-03 14:47:35 +0530

    LSTM: add vanila

--- main.py
+++ main.py
@@ -3,45 +3,42 @@ import torch.nn as nn
 import math
 
 
-class RNN(nn.Module):
-
+class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        self.W_xh = nn.Parameter(torch.randn(input_size, hidden_size))
-        self.W_hh = nn.Parameter(torch.randn(hidden_size, hidden_size))
-
-        self.b_h = nn.Parameter(torch.randn(hidden_size))
+        self.W = nn.Parameter(torch.randn(input_size + hidden_size, 4* hidden_size))
+        self.b = nn.Parameter(torch.randn(4 * hidden_size))
 
         self.reset_parameters()
-
+    
     def reset_parameters(self):
-        std = 1.0 / math.sqrt(self.hidden_size)
+        std = 1.0/math.sqrt(self.hidden_size)
         for weight in self.parameters():
             weight.data.uniform_(-std, std)
+    
+    def forward(self, x_t, h_prev, c_prev):
+        batch_size = x_t.size(0)
 
-    def forward(self, x_t, h_prev):
-        h_t = torch.tanh(x_t @ self.W_xh + h_prev @ self.W_hh + self.b_h)
-        return h_t
+        # Concatenate input and hidden state
+        combined = torch.cat([x_t, h_prev], dim=1)
 
-# smoke test
-input_size = 10
-hidden_size = 20
-batch_size = 3
+        # One matrix multiplication for all gates
+        gates = combined @ self.W + self.b
 
-rnn = RNN(input_size, hidden_size)
+        f_t, i_t, g_t, o_t = gates.chunk(4, dim=1)
 
-h_0 = torch.zeros(batch_size, hidden_size)
+        f_t = torch.sigmoid(f_t) # Forget Gate
+        i_t = torch.sigmoid(i_t) # Input Gate
+        g_t = torch.tanh(g_t) # Candidate cell state
+        o_t = torch.sigmoid(o_t) # Output gate
 
-sequence_length = 5
-x_sequence = torch.randn(sequence_length, batch_size, input_size)
+        # Update cell state
+        c_t = f_t * c_prev + i_t * g_t
 
-h_t = h_0
-for t in range(sequence_length):
-    x_t = x_sequence[t]
-    h_t = rnn(x_t, h_t)
-    print(f"Timestep {t}: h_t shape = {h_t.shape}")
+        # Update hidden state
+        h_t = o_t * torch.tanh(c_t)
 
-print(f"\nFinal hidden state: {h_t.shape}")
+        return h_t, c_t
